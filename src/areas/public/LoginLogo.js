a6cd7e33c8b089c9ff14cab38d25e69de4630119
Paul Stephenson <paul@impel.co>
2020-02-07 01:13:09 +1100

    Login logo remove break.

--- src/areas/public/LoginLogo.js
+++ src/areas/public/LoginLogo.js
@@ -4,6 +4,6 @@ import { Button, Alert, Clearfix, Panel } from 'react-bootstrap'
 export default () => (
   <Clearfix className="login-logo">
     <div className="login-logo__title">Stack<span className="login-logo__shade">Hat</span></div>
-    <br /><br />
+    <br />
   </Clearfix>
 )
